58d64258891134c59d7ce81c9884e2dcc657e95a
Muhammadali <mukhammadaliweb@gmail.com>
2024-08-09 22:18:21 +0500

    Removed unnecessary codes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import bot from "./core/bot.js";
 import { stage } from "./scenes/index.js";
 import connectDB from "./config/connect.db.js";
 import i18nInitilization from "./middlewares/i18n.middleware.js";
-import i18n from "./config/i18n.config.js";
 import isAuth from "./middlewares/auth.middleware.js";
 
 bot.use(session());
