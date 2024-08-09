8360596bf963705c6123f19918b2016aa9d7bb54
Muhammadali <mukhammadaliweb@gmail.com>
2024-08-09 22:25:13 +0500

    Removed unnecessary codes

--- src/scenes/start.js
+++ src/scenes/start.js
@@ -1,5 +1,5 @@
 import { BaseScene } from "telegraf/scenes";
-import { backInlineKeyboard, buttons, aboutUsKeyboard, ourLocationsKeyboard, startKeyboard } from "../utils/keyboards.js";
+import { buttons, startKeyboard } from "../utils/keyboards.js";
 import i18n from "../config/i18n.config.js";
 const startScene = new BaseScene("start")
 
