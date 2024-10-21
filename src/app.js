c5563498ca4c1b864ae527acb0bc427c72f38889
Muhammadali <mukhammadaliweb@gmail.com>
2024-10-21 16:53:20 +0500

    Added secret path with uuid on webhook

--- src/app.js
+++ src/app.js
@@ -2,28 +2,34 @@ import "./index.js";
 import express from "express";
 import environments from "./config/environments.js";
 import bot from "./core/bot.js";
+import * as uuid from "uuid";
 
 const app = express();
+const secretPath = uuid.v4();
 
 app.use(express.json());
 
-app.use('/dilkashdeliverybot', bot.webhookCallback('/secret-path'))
-bot.telegram.setWebhook(environments.SERVER_URL)
+app.use("/dilkashdeliverybot", bot.webhookCallback(secretPath));
+bot.telegram.setWebhook(environments.SERVER_URL + secretPath);
 
 app.get("/dilkashdeliverybot", (req, res) => {
-    res.status(200).json({
-        ok: true,
-        message: "Bot running..."
-    });
+  res.status(200).json({
+    ok: true,
+    message: "Bot running...",
+  });
 });
 
 app.use((req, res) => {
-    res.status(404).json({
-        ok: false,
-        message: "Page not found"
-    });
+  res.status(404).json({
+    ok: false,
+    message: "Page not found",
+  });
 });
 
 app.listen(environments.PORT, () => {
-    console.log(`Bot launched on ${ new Date().toLocaleString("uz") } on port ${environments.PORT}`);
+  console.log(
+    `Bot launched on ${new Date().toLocaleString("uz")} on port ${
+      environments.PORT
+    }`
+  );
 });
